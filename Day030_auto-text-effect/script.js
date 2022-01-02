b6b393b7847fe643c324f5c33b422737f3aaf3e3
tranthaituananh <19521219@gm.uit.edu.vn>
2022-01-03 06:36:10 +0700

    Day 30

---
+++ Day030_auto-text-effect/script.js
@@ -0,0 +1,21 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'Hey there, My name is Tuan Anh and I am a frontend developer.'
+let idx = 1
+let speed = 300 / speedEl.value
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+writeText()
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
